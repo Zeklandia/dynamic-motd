89d9ee580e0ca1852055faee80e658b47c22d300
Zeklandia <awood5659@gmail.com>
2013-03-23 13:19:09 -0400

    Start C++ version project

---
+++ C++/Dynamic MOTD (C)/dynmotd.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   dynmotd.cpp
+ * Author: asher
+ *
+ * Created on March 23, 2013, 1:18 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+     return 0;
+}
+
