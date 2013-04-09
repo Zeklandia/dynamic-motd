bf2f00424d638101dd5c4c5717279a36fb77f6b9
Zeklandia <awood5659@gmail.com>
2013-04-09 13:04:49 -0400

    Unify variable names, work on C++, clean up custom command function Signed-off-by: Zeklandia <awood5659@gmail.com>

--- C++/Dynamic MOTD (C)/dynmotd.cpp
+++ C++/Dynamic MOTD (C)/dynmotd.cpp
@@ -25,21 +25,26 @@ using namespace std;
 int main()
 {
      //Begin loading information
-     std::cout << "Loading...\n";
+     std::cout << "Loading..." << endl <<;
      system("reset");
 
      //Load Settings
      /*
-     CONFIGFILE=~/.config/dynmotd.conf
-     if [ ! -d ~/.config ];
-     then
-         mkdir ~/.config
-     fi
+     # Create configuration file if non-existant
+
      if [ ! -f $CONFIGFILE ];
      then
-         echo -e # Set options for main script\n# ZIP Code (For weather)\nZIP=11234\n# Print banner (1=yes, 0=no)\nPRINTBAN=1\n# Which banner to print (BANLINUX, BANLINUXBIG, BANUBUNTU, BANANDROID, or BANMACOSX)\nBANNER=BANLINUX >>$CONFIGFILE
+         echo -e "# Set options for main script\n# Turn debugging mode on or off (1 to enable, 0 to disable\nDEBUG=0\n# Color for Output\nTITLE_COLOR=\"$WHITE_BOLD\"\nHEADER_COLOR=\"$RED_BOLD\"\nBORDER_COLOR=\"$CYAN\"\nOUTPUT_COLOR=\"$GREEN_BOLD\"\n# Print full output of the command w or only the first line (1=full output, 0=first line only)\nFULLW=0\n# Print weather information (1=yes, 0=no)\nWEATHER=1\n# WEATHER_ZIP Code (For weather)\nWEATHER_ZIP=11234\n# Print banner (1=yes, 0=no)\nPRINTBAN=1\n# Print a custom banner (1=yes, 0=no)\nCUSTBAN=0\n# Text to be displayed in custom banner (backticks and \ escapes are allowed, use a backslash followed by the letter n to end a line(\ n without the space))\nCUSTBANTEXT=\"\"\n# Which banner to print (BANLINUX, BANLINUXBIG, BANUBUNTU, BANANDROID, or BANMACOSX)\nBANNER=BANLINUX\n# Custom Command Section (Set CUSTOM to 1 to activate, 0 to disable)\nCUSTOM=0\nCUSTOM_HEADER=\"Custom Command Stats\"\nCUSTOM_TITLE=\"Custom Command\"\nCUSTOM_COMMAND=\"echo Custom Command Output\"" >> $CONFIGFILE
      fi
-     source $CONFIGFILE
+
+     # Link executable if not already done
+
+     if [ ! -f /usr/bin/dynmotd ];
+     then
+         ln -s "/usr/local/bin/dynmotd" "/usr/bin/"
+     fi
+
+     source $CONFIGFILE                                                                                 # Load settings
      */
      //Also need to link this file to /usr/bin/dynmotd-c
 
@@ -64,135 +69,153 @@ int main()
      green_bold = "\033[1;32m";
 
      //System Stats
-     struct utsname u;
-     uname(&u);
+     struct utsname uname_info;
+     uname(&uname_info);
      const long minute = 60;
      const long hour = minute * 60;
      const long day = hour * 24;
      const double megabyte = 1024 * 1024;
-     struct sysinfo si;
-     sysinfo (&si);
+     struct sysinfo uname_system_info;
+     sysinfo (&uname_system_info);
 
      //Get time
-     time_t rawtime;
-     time ( &rawtime );
+     time_t hex_time;
+     time ( &hex_time );
      string datetime;
-     datetime = ctime( &rawtime );
+     datetime = ctime( &hex_time );
 
      //Get hostname
      char hostname[128];
      gethostname(hostname, sizeof hostname);
 
      //Get WLAN IP address
-     string wlanip;
-     int wlanipaddr;
-     struct ifreq wlanipnumaddr;
-     wlanipaddr = socket(AF_INET, SOCK_DGRAM, 0);
-     wlanipnumaddr.ifr_addr.sa_family = AF_INET;
-     strncpy(wlanipnumaddr.ifr_name, "wlan0", IFNAMSIZ-1);
-     ioctl(wlanipaddr, SIOCGIFADDR, &wlanipnumaddr);
-     close(wlanipaddr);
-     wlanip = inet_ntoa(((struct sockaddr_in *)&wlanipnumaddr.ifr_addr)->sin_addr);
+     string wlan_ip;
+     int wlan_ip_address;
+     struct ifreq wlan_ip_address_numeric;
+     wlan_ip_address = socket(AF_INET, SOCK_DGRAM, 0);
+     wlan_ip_address_numeric.ifr_addr.sa_family = AF_INET;
+     strncpy(wlan_ip_address_numeric.ifr_name, "wlan0", IFNAMSIZ-1);
+     ioctl(wlan_ip_address, SIOCGIFADDR, &wlan_ip_address_numeric);
+     close(wlan_ip_address);
+     wlan_ip = inet_ntoa(((struct sockaddr_in *)&wlan_ip_address_numeric.ifr_addr)->sin_addr);
 
      //Get LAN IP address
-     string lanip;
-     int lanipaddr;
-     struct ifreq lanipnumaddr;
-     lanipaddr = socket(AF_INET, SOCK_DGRAM, 0);
-     lanipnumaddr.ifr_addr.sa_family = AF_INET;
-     strncpy(lanipnumaddr.ifr_name, "eth0", IFNAMSIZ-1);
-     ioctl(lanipaddr, SIOCGIFADDR, &lanipnumaddr);
-     close(lanipaddr);
-     lanip = inet_ntoa(((struct sockaddr_in *)&lanipnumaddr.ifr_addr)->sin_addr);
+     string lan_ip;
+     int lan_ip_address;
+     struct ifreq lan_ip_address_numeric;
+     lan_ip_address = socket(AF_INET, SOCK_DGRAM, 0);
+     lan_ip_address_numeric.ifr_addr.sa_family = AF_INET;
+     strncpy(lan_ip_address_numeric.ifr_name, "eth0", IFNAMSIZ-1);
+     ioctl(lan_ip_address, SIOCGIFADDR, &lan_ip_address_numeric);
+     close(lan_ip_address);
+     lan_ip = inet_ntoa(((struct sockaddr_in *)&lan_ip_address_numeric.ifr_addr)->sin_addr);
 
      //Get kernel version
-     string kernelver;
-     kernelver = u.release;
+     string kernel_version;
+     kernel_version = uname_info.release;
 
      //Get RAM size in kB
      //MEMSIZE=`cat /proc/meminfo | grep MemTotal | awk {'print $2'}`
-     //printf ("total RAM   : %5.1f MB\n", si.totalram / megabyte);
-     string memsize;
-     memsize = "RAM";
+     //printf ("total RAM   : %5.1f MB" << endl <<, si.totalram / megabyte);
+     string ram_size;
+     ram_size = "RAM";
 
      //Get current users, jobs, login times, and locations
      //USERSTATS=`w`
-     /*printf ("system uptime : %ld days, %ld:%02ld:%02ld\n",
+     /*printf ("system uptime : %ld days, %ld:%02ld:%02ld" << endl <<,
      si.uptime / day, (si.uptime % day) / hour,
      (si.uptime % hour) / minute, si.uptime % minute);*/
-     string stats;
-     stats = system("w | awk 'NR==1' | cut -c2-");
+     string user_stats;
+     user_stats = system("w | awk 'NR==1' | cut -c2-");
 
      //Get username
-     string user;
-     user = getenv("USER");
+     string username;
+     username = getenv("USER");
 
      //Get time, location, length, and other details for last login
      //LASTLOGIN=`last -n 2 -R $USER | awk 'NR==2'`
-     string last;
-     last = system("last -n 2 -R $USER | awk 'NR==2'");
+     string last_login;
+     last_login = system("last -n 2 -R $USER | awk 'NR==2'");
 
      //Get home directory path
-     string homedir;
-     homedir = getenv("HOME");
+     string home_path;
+     home_path = getenv("HOME");
 
      //Get the current number of this user's active processes
      //PROCOUNT=`ps -Afl | wc -l`
-     //printf ("process count : %d\n", si.procs);
-     string proccount;
-     proccount = (si.procs);
+     //printf ("process count : %d" << endl <<, si.procs);
+     string process_count;
+     process_count = (uname_system_info.procs);
 
      //Get this user's limit on processes
      //PROLIMIT=`ulimit -u`
-     string proclimit;
-     proclimit = system("cat /proc/meminfo | grep MemTotal | awk {'print $2'}");
+     string process_limit;
+     process_limit = system("cat /proc/meminfo | grep MemTotal | awk {'print $2'}");
 
      //Print the location and ZIP code being used for weather
      //WLOC=`weather $ZIP | grep 'Current conditions at' | awk '{print $4}'`
-     string wloc;
-     wloc = "location";
-     string wzip;
-     wzip = "zip";
+     string weather_location;
+     weather_location = "location";
+     string weather_zip;
+     weather_zip = "zip";
 
      //Get the temperature
      //WTEMP=`weather $ZIP | grep 'Temperature:' | awk '{print $2}'`
-     string wtemp;
-     wtemp = "Temperature";
+     string weather_temperature;
+     weather_temperature = "Temperature";
 
      //Get the weather conditions (if there are any)
      //WCOND=`weather $ZIP | grep 'Weather:' | cut -d: -f2 | cut -c2-`
-     string wcond;
-     wcond = "Weather conditions";
+     string weather_conditions;
+     weather_conditions = "Weather conditions";
 
      //Get the sky conditions
      //WSKY=`weather $ZIP | grep 'Sky conditions:' | cut -d: -f2 | cut -c2-`
-     string wsky;
-     wsky = "Sky conditions";
+     string weather_sky_conditions;
+     weather_sky_conditions = "Sky conditions";
+
+     //Custom section
+     string custom_header;
+     custom_header = "Custom Command Stats";
+
+     string custom_title;
+     custom_title = "Custom Command";
+
+     string custom_command;
+     custom_command = "echo -e Custom Command";
+
+     string custom_command_output;
+     custom_command_output = system(custom_command);
 
      //Finished!
-     std::cout << "Done!\n";
+     std::cout << "Done!";
      system("reset");
 
      //Print the loaded information
-     std::cout << "" << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=[ " << red_bold << "System Stats" << cyan << " ]=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:\n"
+     std::cout << "" << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=[ " << red_bold << "System Stats" << cyan << " ]=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl <<
              "" << cyan << ":   " << white_bold << "Date/Time" << cyan << " = " << green_bold << datetime << ""
-             "" << cyan << ":    " << white_bold << "Hostname" << cyan << " = " << green_bold << hostname << "\n"
-             "" << cyan << ":     " << white_bold << "WLAN IP" << cyan << " = " << green_bold << wlanip << "\n"
-             "" << cyan << ":      " << white_bold << "LAN IP" << cyan << " = " << green_bold << lanip << "\n"
-             "" << cyan << ":      " << white_bold << "Kernel" << cyan << " = " << green_bold << kernelver << "\n"
-             "" << cyan << ":      " << white_bold << "Memory" << cyan << " = " << green_bold << memsize << " mB\n"
-             "" << cyan << ":       " << white_bold << "Stats" << cyan << " = " << green_bold << stats << "\n"
-             "" << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:[ " << red_bold << "User Stats" << cyan << " ]:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:\n"
-             "" << cyan << ":    " << white_bold << "Username" << cyan << " = " << green_bold << user << "\n"
-             "" << cyan << ":  " << white_bold << "Last Login" << cyan << " = " << green_bold << last << "\n"
-             "" << cyan << ":        " << white_bold << "Home" << cyan << " = " << green_bold << homedir << "\n"
-             "" << cyan << ":   " << white_bold << "Processes" << cyan << " = " << green_bold << proccount << " of " << proclimit << "\n";
-     std::cout << "" << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:[ " << red_bold << "Internet Weather Stats" << cyan << " ]:=:=:=:=:=:=:=:=:=:=:=:=:=:\n"
-             "" << cyan << ":    " << white_bold << "Location" << cyan << " = " << green_bold << wloc << ", " << wzip << "\n"
-             "" << cyan << ":        " << white_bold << "Temp" << cyan << " = " << green_bold << wtemp << "\n"
-             "" << cyan << ":     " << white_bold << "Weather" << cyan << " = " << green_bold << wcond << "\n"
-             "" << cyan << ":         " << white_bold << "Sky" << cyan << " = " << green_bold << wsky << "\n";
-     std::cout << "" << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:[]:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:"
+             "" << cyan << ":    " << white_bold << "Hostname" << cyan << " = " << green_bold << hostname << endl
+             "" << cyan << ":     " << white_bold << "WLAN IP" << cyan << " = " << green_bold << wlan_ip << endl
+             "" << cyan << ":      " << white_bold << "LAN IP" << cyan << " = " << green_bold << lan_ip << endl
+             "" << cyan << ":      " << white_bold << "Kernel" << cyan << " = " << green_bold << kernel_version << endl
+             "" << cyan << ":      " << white_bold << "Memory" << cyan << " = " << green_bold << ram_size << " mB" << endl <<
+             "" << cyan << ":       " << white_bold << "Stats" << cyan << " = " << green_bold << user_stats << endl
+             "" << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:[ " << red_bold << "User Stats" << cyan << " ]:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl <<
+             "" << cyan << ":    " << white_bold << "Username" << cyan << " = " << green_bold << username << endl
+             "" << cyan << ":  " << white_bold << "Last Login" << cyan << " = " << green_bold << last_login << endl
+             "" << cyan << ":        " << white_bold << "Home" << cyan << " = " << green_bold << home_path << endl
+             "" << cyan << ":   " << white_bold << "Processes" << cyan << " = " << green_bold << process_count << " of " << process_limit << endl;
+
+     std::cout << "" << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:[ " << red_bold << "Internet Weather Stats" << cyan << " ]:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl <<
+             "" << cyan << ":    " << white_bold << "Location" << cyan << " = " << green_bold << weather_location << ", " << weather_zip << endl
+             "" << cyan << ":        " << white_bold << "Temp" << cyan << " = " << green_bold << weather_temperature << endl
+             "" << cyan << ":     " << white_bold << "Weather" << cyan << " = " << green_bold << weather_conditions << endl
+             "" << cyan << ":         " << white_bold << "Sky" << cyan << " = " << green_bold << weather_sky_conditions << endl;
+
+     std::cout << "" << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=[ " << red_bold << custom_header << cyan << " ]=:=:=:=:=:=:=:=:=:=:=:=:=:=:" << endl <<
+             "" << cyan << ":    " << white_bold << custom_command << cyan << " = " << green_bold << custom_command_output << endl
+
+             std::cout << "" << cyan << ":=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:[]:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:"
              "" << color_default << endl << endl;
      return 0;
 }
